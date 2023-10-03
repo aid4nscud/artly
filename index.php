3df3e3d2cbac6f0d4e6204277f667dcb42506325
Aidan Scudder <aidan.scudder@gmail.com>
2023-10-02 22:51:10 -0600

    created skeleton of file structure

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Artly - Home</title>
+    <link rel="stylesheet" href="styles/index.css">
+</head>
+<body>
+    <header class="main-header">
+        <div class="logo">
+            ARTLY
+        </div>
+        <nav class="main-nav">
+            <ul class="nav-list">
+                <li class="nav-item"><a href="./artist/dashboard.php">Artists</a></li>
+                <li class="nav-item"><a href="./collector/dashboard.php">Collectors</a></li>
+                <li class="nav-item"><a href="./admin/dashboard.php">Admin</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="main-content">
+        <h1 class="main-title">Welcome to Artly</h1>
+        <p class="intro-text">This is a platform for artists and art collectors to sell and buy art</p>
+    </main>
+
+    <footer class="main-footer">
+        <p>Copyright 2023, Aidan Scudder</p>
+    </footer>
+</body>
+</html>
