bddc1139ead770b5dc69ede67bf9053d4ba6e32b
Aidan Scudder <aidan.scudder@gmail.com>
2023-10-03 00:49:56 -0600

    finished artist and collector sections

---
+++ collector/browse-art.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Browse Art</title>
+    <link rel="stylesheet" href="../styles/index.css">
+    <link rel="stylesheet" href="../styles/browse-art.css">
+</head>
+<body>
+
+    <header class="main-header">
+        <div class="logo">ARTLY</div>
+        <nav class="main-nav">
+            <ul class="nav-list">
+                <li class="nav-item"><a href="./dashboard.php">Dashboard</a></li>
+                <li class="nav-item"><a href="./browse-art.php">Browse Art</a></li>
+                <li class="nav-item"><a href="./my-bids.php">My Bids</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="main-content">
+        <h1 class="main-title">Browse Art</h1>
+
+        <section class="art-grid">
+            <div class="art-card">
+                <img src="../images/artist1.webp" alt="Artist1" class="artist-profile">
+                <img src="../images/sample1.webp" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 1</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+            <div class="art-card">
+                <img src="../images/artist2.webp" alt="Artist1" class="artist-profile">
+                <img src="../images/sample2.jpeg" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 2</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+            <div class="art-card">
+                <img src="../images/artist3.jpeg" alt="Artist1" class="artist-profile">
+                <img src="../images/sample3.jpeg" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 3</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+            <div class="art-card">
+                <img src="../images/artist4.jpeg" alt="Artist1" class="artist-profile">
+                <img src="../images/sample4.webp" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 4</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+            <div class="art-card">
+                <img src="../images/artist1.webp" alt="Artist1" class="artist-profile">
+                <img src="../images/sample1.webp" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 1</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+            <div class="art-card">
+                <img src="../images/artist4.jpeg" alt="Artist1" class="artist-profile">
+                <img src="../images/sample5.webp" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 2</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+            <div class="art-card">
+                <img src="../images/artist3.jpeg" alt="Artist1" class="artist-profile">
+                <img src="../images/sample3.jpeg" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 3</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+            <div class="art-card">
+                <img src="../images/artist4.jpeg" alt="Artist1" class="artist-profile">
+                <img src="../images/sample4.webp" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 4</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+            <div class="art-card">
+                <img src="../images/artist1.webp" alt="Artist1" class="artist-profile">
+                <img src="../images/sample1.webp" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 1</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+            <div class="art-card">
+                <img src="../images/artist4.jpeg" alt="Artist1" class="artist-profile">
+                <img src="../images/sample5.webp" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 2</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+            <div class="art-card">
+                <img src="../images/artist3.jpeg" alt="Artist1" class="artist-profile">
+                <img src="../images/sample3.jpeg" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 3</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+            <div class="art-card">
+                <img src="../images/artist4.jpeg" alt="Artist1" class="artist-profile">
+                <img src="../images/sample4.webp" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 4</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <button class="buy-button">Buy/Bid</button>
+                </div>
+            </div>
+      
+        </section>
+
+    </main>
+
+    <footer class="main-footer">
+        <p>Copyright 2023, Aidan Scudder</p>
+    </footer>
+
+</body>
+</html>
---
+++ collector/my-bids.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>My Bids</title>
+    <link rel="stylesheet" href="../styles/index.css">
+    <link rel="stylesheet" href="../styles/my-bids.css">
+</head>
+
+<body>
+
+    <header class="main-header">
+        <div class="logo">ARTLY</div>
+        <nav class="main-nav">
+            <ul class="nav-list">
+                <li class="nav-item"><a href="./dashboard.php">Dashboard</a></li>
+                <li class="nav-item"><a href="./browse-art.php">Browse Art</a></li>
+                <li class="nav-item"><a href="./my-bids.php">My Bids</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="main-content">
+        <h1 class="main-title">My Bids</h1>
+
+
+        <section class="art-grid">
+            <div class="art-card">
+                <div class="final-price">$5400</div>
+                <img src="../images/sample4.webp" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 1</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <div class="status-label">Bought</div>
+                </div>
+            </div>
+
+        
+            <div class="art-card">
+                <div class="final-price">$500</div>
+                <img src="../images/sample2.jpeg" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 2</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <div class="status-label expired">Expired</div>
+                </div>
+            </div>
+
+    
+            <div class="art-card">
+                <div class="final-price">$1500</div>
+                <img src="../images/sample3.jpeg" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 3</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <div class="status-label expired">Expired</div>
+                </div>
+            </div>
+
+     
+            <div class="art-card">
+                <div class="final-price">$575</div>
+                <img src="../images/sample4.webp" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 4</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <div class="status-label">Bought</div>
+                </div>
+            </div>
+
+       
+            <div class="art-card">
+                <div class="final-price">$200</div>
+                <img src="../images/sample1.webp" alt="Art1" class="art-image">
+                <div class="art-details">
+                    <h3>Art Piece 5</h3>
+                    <p>Abstract art piece made with oil paints.</p>
+                    <div class="status-label">Bought</div>
+                </div>
+            </div>
+
+        </section>
+
+    </main>
+
+    <footer class="main-footer">
+        <p>Copyright 2023, Aidan Scudder</p>
+    </footer>
+
+</body>
+
+</html>
