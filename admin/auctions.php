8a161fd6ccd8ac778f3f59cc8a6d8d28bf3d17f0
Aidan Scudder <aidan.scudder@gmail.com>
2023-10-09 18:03:50 -0600

    added auction page

---
+++ admin/auctions.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Auctions</title>
+    <link rel="stylesheet" href="../styles/index.css">
+    <link rel="stylesheet" href="../styles/admin-auctions.css">
+</head>
+<body>
+
+    <header class="main-header">
+        <div class="logo">ARTLY</div>
+        <nav class="main-nav">
+            <ul class="nav-list">
+                <li class="nav-item"><a href="./dashboard.php">Dashboard</a></li>
+                <li class="nav-item"><a href="./users.php">Users</a></li>
+                <li class="nav-item"><a href="./art.php">Art</a></li>
+                <li class="nav-item"><a href="./auctions.php">Auctions</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="main-content">
+        <h1 class="main-title">Current Auctions</h1>
+
+  
+        <section class="auctions-grid">
+
+    <div class="auction-card">
+        <img src="../images/artist3.jpeg" alt="Artist" class="artist-profile">
+        <img src="../images/sample3.jpeg" alt="Art1" class="auction-image">
+        <div class="auction-details">
+            <h3>Art Piece 1</h3>
+            <p>5 bids</p>
+            <p>2 days left</p>
+        </div>
+        <button class="action-button">Actions</button>
+    </div>
+    <div class="auction-card">
+        <img src="../images/artist3.jpeg" alt="Artist" class="artist-profile">
+        <img src="../images/sample3.jpeg" alt="Art1" class="auction-image">
+        <div class="auction-details">
+            <h3>Art Piece 1</h3>
+            <p>5 bids</p>
+            <p>2 days left</p>
+        </div>
+        <button class="action-button">Actions</button>
+    </div>
+    <div class="auction-card">
+        <img src="../images/artist1.webp" alt="Artist" class="artist-profile">
+        <img src="../images/sample1.webp" alt="Art1" class="auction-image">
+        <div class="auction-details">
+            <h3>Art Piece 1</h3>
+            <p>5 bids</p>
+            <p>2 days left</p>
+        </div>
+        <button class="action-button">Actions</button>
+    </div>
+    <div class="auction-card">
+        <img src="../images/artist3.jpeg" alt="Artist" class="artist-profile">
+        <img src="../images/sample3.jpeg" alt="Art1" class="auction-image">
+        <div class="auction-details">
+            <h3>Art Piece 1</h3>
+            <p>5 bids</p>
+            <p>2 days left</p>
+        </div>
+        <button class="action-button">Actions</button>
+    </div>
+    <div class="auction-card">
+        <img src="../images/artist1.webp" alt="Artist" class="artist-profile">
+        <img src="../images/sample1.webp" alt="Art1" class="auction-image">
+        <div class="auction-details">
+            <h3>Art Piece 1</h3>
+            <p>5 bids</p>
+            <p>2 days left</p>
+        </div>
+        <button class="action-button">Actions</button>
+    </div>
+    <div class="auction-card">
+        <img src="../images/artist3.jpeg" alt="Artist" class="artist-profile">
+        <img src="../images/sample3.jpeg" alt="Art1" class="auction-image">
+        <div class="auction-details">
+            <h3>Art Piece 1</h3>
+            <p>5 bids</p>
+            <p>2 days left</p>
+        </div>
+        <button class="action-button">Actions</button>
+    </div>
+    <div class="auction-card">
+        <img src="../images/artist1.webp" alt="Artist" class="artist-profile">
+        <img src="../images/sample1.webp" alt="Art1" class="auction-image">
+        <div class="auction-details">
+            <h3>Art Piece 1</h3>
+            <p>5 bids</p>
+            <p>2 days left</p>
+        </div>
+        <button class="action-button">Actions</button>
+    </div>
+
+    
+</section>
+
+    </main>
+
+    <footer class="main-footer">
+        <p>Copyright 2023, Aidan Scudder</p>
+    </footer>
+</body>
+</html>
